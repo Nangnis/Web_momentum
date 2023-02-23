51c14b1788923693c4f214675c5a04145697f9dd
Naeun Shin <95427010+Nangnis@users.noreply.github.com>
2023-02-23 14:10:04 +0900

    Add files via upload

---
+++ momentum/js/quotes.js
@@ -0,0 +1,67 @@
+const quotes = [
+  {
+    quote_en: "Life is not fair; get used to it.",
+    quote_ko: "인생이란 결코 공평하지 않다. 이 사실에 익숙해져라.",
+    author: "Bill Gates (빌 게이츠)",
+  },
+  {
+    quote_en:
+      "We always overestimate the change that will occur in the next two years and underestimate the change that will occur in the next ten. Don't let yourself be lulled into inaction.",
+    quote_ko:
+      "우리는 앞으로 2년 뒤에 닥쳐올 변화에 대해서는 과대평가하지만 10년 뒤에 올 변화는 과소평가하는 경향이 있다. 그렇다고 스스로를 나태함으로 이끌지는 마라.",
+    author: "Bill Gates (빌 게이츠)",
+  },
+  {
+    quote_en:
+      "The first step to getting the things you want out of life is this: Decide what you want.",
+    quote_ko:
+      "인생에서 원하는 것을 얻기 위한 첫 번째 단계는 내가 무엇을 원하는지 결정하는 것이다.",
+    author: "Ben Stein (벤 스타인)",
+  },
+  {
+    quote_en: "No man is born wise.",
+    quote_ko: "태어나면서부터 현명한 이는 없다.",
+    author: "Miguel de Cervantes (미겔 데 세르반테스)",
+  },
+  {
+    quote_en: "A goal without a plan is just a wish.",
+    quote_ko: "계획 없는 목표는 한낱 꿈에 불과하다.",
+    author: "Antoine de Saint-Exupery (생텍쥐페리)",
+  },
+  {
+    quote_en:
+      "Surround yourself with only people who are going to lift you higher.",
+    quote_ko: "여러분을 더욱 높이 올려줄 사람만을 가까이 하세요.",
+    author: "Oprah Winfrey (오프라 윈프리)",
+  },
+  {
+    quote_en: "Adversity does teach who your real friends are.",
+    quote_ko: "역경은 누가 진정한 친구인지 가르쳐준다.",
+    author: "Lois McMaster Bujold (로이스 맥마스터 부욜)",
+  },
+  {
+    quote_en: "Punctuality is the soul of business.",
+    quote_ko: "시간 엄수는 비즈니스의 영혼이다.",
+    author: "Thomas Halyburton (토마스 할리버튼)",
+  },
+  {
+    quote_en:
+      "Challenges are what make life interesting; overcoming them is what makes life meaningful.",
+    quote_ko:
+      "도전은 인생을 흥미롭게 만들며, 도전의 극복이 인생을 의미있게 한다.",
+    author: "Joshua J. Marine (조슈아 J. 마린)",
+  },
+  {
+    quote_en:
+      "Once you agree upon the price you and your family must pay for success, it enables you to ignore the minor hurts, the opponent's pressure, and the temporary failures.",
+    quote_ko:
+      "일단 당신이 성공을 위해 자신과 가족이 치뤄야 할 대가를 인정하면 사소한 아픔, 경쟁자의 압력, 일시적 실패는 감내할 수 있다.",
+    author: "Vincent Lombardi (빈센트 롬바르디)",
+  },
+  {
+    quote_en:
+      "No man is good enough to govern another man without that other's consent.",
+    quote_ko: "누구도 본인의 동의없이 남을 지배할 만큼 훌륭하지는 않다.",
+    author: "Abraham Lincoln (에이브러햄 링컨)",
+  },
+];
